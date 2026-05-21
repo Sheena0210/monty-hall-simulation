7795242e41727467fa2c4b6b1ccf0e59019b90c8
TSAI-SYUAN,LIN <aaaa920102@gmail.com>
2026-05-21 23:23:09 +0800

    新增第二個control

--- 第二次的選擇.R
+++ 第二次的選擇.R
@@ -31,13 +31,13 @@ var(y)
 #(2)如果主持人不知道跑車在哪----
 set.seed(1)
 
-#first_car:1=第一次選到車 0=第一次沒選到車
-first_car <- function(n = 1){
-  p <- 1/3
-  u <- runif(n)
-  x <- as.integer(u > 1 - p)
-  return(x)
-}
+  #first_car:1=第一次選到車 0=第一次沒選到車
+  first_car <- function(n = 1){
+    p <- 1/3
+    u <- runif(n)
+    x <- as.integer(u > 1 - p)
+    return(x)
+  }
 
 
 #(2)主持人不知道車在哪裡，若第一次沒選到車，主持人選到羊p=1/2,此時換門後是車機率:0.33532(1/3)----
@@ -46,21 +46,38 @@ first_car <- function(n = 1){
 
 #1:換門後是車 0:換門後是羊
 change_door_unknown <- function(x){
-  #第一次選到車
-  if(x == 1){
-    y <- 0
-  }else{
-  #第一次沒選到車 主持人也不知道正確答案 選到羊的機率1/2
-  open_goat <- rbinom(1, size = 1, prob = 1/2)
-  if(open_goat == 1){
-    y <- 1
-    }else{
-    y <- 0
+  y <- numeric(length(x))
+  for(i in 1:length(x)){
+    if(x[i] == 1){
+      y[i] <- 0
+    }
+    else{
+      open_goat <- rbinom(1, size = 1, prob = 1/2) #改
+      y[i] <- open_goat
     }
   }
+  
+  x_1 = x[x==1]
+  y_1 = numeric(length(x_1))
+  x_2 = x[x!=1]
+  open_goat = rbinom(n = length(x_2),size = 1,prob = 0.5)
+  y = c(y_1,open_goat)
+  
   return(y)
 }
+#第二次control
+change_unknown_control = function(x){
+    x_1 = x[x==1]
+    y_1 = numeric(length(x_1))
+    x_2 = x[x!=1]
+    open_goat = r_2_control(n = length(x_2))
+    y = c(y_1,open_goat)
+    return(y)
+}
 
+x <- rbinom(n = 1000, size = 1, prob = 1/3)
+y = change_unknown_control(x)
+mean(y)
 
 #MC
 n <- 100000
@@ -70,8 +87,6 @@ for(i in 1:n){
   x <- first_car()
   y[i] <- change_door_unknown(x)
 }
-
-
 mean(y)
 var(y)
 
@@ -88,13 +103,13 @@ set.seed(1)
 n <- 10000
 B <- 10000
 p <- 1/3
-# 一開始選到車：inverse function
+# 一開始選到車：inverse function----
 first_car <- function(n = 10000, p = 1/3){
   u <- runif(n)
   x <- as.integer(u > 1 - p)
   return(x)
 }
-# 一開始選到車：control variance
+# 一開始選到車：control variance----
 first_car_control_var <- function(n = 10000, p = 1/3){
   u <- runif(n / 2)
   u_2 <- 1 - u
@@ -108,6 +123,7 @@ change_known <- function(x){
   y <- 1 - x
   return(y)
 }
+
 # 主持人不知道車在哪：換門結果
 # 1 = 換門贏，0 = 換門輸
 change_unknown <- function(x){
@@ -124,7 +140,10 @@ change_unknown <- function(x){
   return(y)
 }
 
-#(3-1)如果主持人知道跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean 分布----
+
+
+
+#(3-1)如果主持人知道跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean分布----
 # R 內建 Bernoulli
 R_default_known_distribution <- function(n = 10000, B = 10000, p = 1/3){
   y <- numeric(B)
@@ -167,22 +186,21 @@ colnames(Compare_known) <- c("Mean", "Var")
 Compare_known
 
 
-#(3-2)如果主持人不知跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean 分布----
+#(3-2)如果主持人不知跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean分布----
 # R 內建 Bernoulli
-R_default_unknown_distribution <- function(n = 10000, B = 10000, p = 1/3){
+R_default_unknown_distribution <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
-  
   for(i in 1:B){
     x <- rbinom(n, size = 1, prob = p)
     result <- change_unknown(x)
     y[i] <- mean(result)
   }
   
-  return(c(mean(y), var(y)))
+  return(y)
 }
 
 # inverse function
-Inverse_unknown_distribution <- function(n = 10000, B = 10000, p = 1/3){
+Inverse_unknown_distribution <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   
   for(i in 1:B){
@@ -195,7 +213,7 @@ Inverse_unknown_distribution <- function(n = 10000, B = 10000, p = 1/3){
 }
 
 # inverse function + controlling var
-Control_unknown_distribution <- function(n = 10000, B = 10000, p = 1/3){
+Control_unknown_distribution <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   
   for(i in 1:B){
@@ -236,14 +254,26 @@ Compare_all
 
 
 
+
+
 #分佈圖----
 #(3-1)如果主持人知道跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean----
 change_known <- function(x){
   result <- 1 - x
   return(result)
 }
-# R 內建 Bernoulli
-R_default_known_y <- function(n = 10000, B = 10000, p = 1/3){
+R_default_unknown_distribution <- function(n = 10000, B = 10000, p = 1/3){
+  y <- numeric(B)
+  
+  for(i in 1:B){
+    x <- rbinom(n, size = 1, prob = p)
+    result <- change_unknown_control(x)
+    y[i] <- mean(result)
+  }
+  
+  return(c(mean(y), var(y)))
+}# R 內建 Bernoulli
+R_default_known_y <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   for(i in 1:B){
     x <- rbinom(n, size = 1, prob = p)
@@ -271,7 +301,7 @@ text(x = mean(R_default_known_y) + 0.002,
 
 
 # Inverse function
-Inverse_known_y <- function(n = 10000, B = 10000, p = 1/3){
+Inverse_known_y <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   for(i in 1:B){
     x <- first_car(n, p)
@@ -300,7 +330,7 @@ text(x = mean(Inverse_known_y) + 0.002,
 
 
 # Inverse function + antithetic variates
-Control_known_y <- function(n = 10000, B = 10000, p = 1/3){
+Control_known_y <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   for(i in 1:B){
     x <- first_car_control_var(n, p)
@@ -358,35 +388,26 @@ legend("topright",
 #(3-2)如果主持人不知跑車在哪，比較三種情況（r內建、inverse method、antithetic）sample mean----
 
 # R 內建 Bernoulli
-R_default_unknown_y <- function(n = 10000, B = 10000, p = 1/3){
-  y <- numeric(B)
-  for(i in 1:B){
-    x <- rbinom(n, size = 1, prob = p)
-    result <- change_unknown(x)
-    y[i] <- mean(result)
-  }
- 
-  return(y)
-}
-R_default_unknown_y <- R_default_unknown_y(n, B, p)
-hist(R_default_unknown_y,
+
+R_default_unknown_distribution  <-R_default_unknown_distribution(n, B, p)
+hist(R_default_unknown_distribution ,
      main = "Sampling Distribution of the Estimated Winning Probability 
      (R Bernoulli Simulation)",
      xlab = "Estimated winning probability",
      probability = TRUE)
 
-abline(v = mean(R_default_unknown_y),
+abline(v = mean(R_default_unknown_distribution ),
        col = "yellow",
        lty = 2,
        lwd = 2)
-text(x = mean(R_default_unknown_y) + 0.002,
-     y = max(hist(R_default_unknown_y, plot = FALSE)$density) * 0.9,
-     labels = paste0("Mean = ", round(mean(R_default_unknown_y), 4)),
+text(x = mean(R_default_unknown_distribution ) + 0.002,
+     y = max(hist(R_default_unknown_distribution , plot = FALSE)$density) * 0.9,
+     labels = paste0("Mean = ", round(mean(R_default_unknown_distribution ), 4)),
      pos = 4)
 
 
 # Inverse function
-Inverse_unknown_y <- function(n = 10000, B = 10000, p = 1/3){
+Inverse_unknown_y <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   for(i in 1:B){
     x <- first_car(n, p)
@@ -409,10 +430,10 @@ text(x = mean(Inverse_unknown_y) + 0.002,
      y = max(hist(Inverse_unknown_y, plot = FALSE)$density) * 0.9,
      labels = paste0("Mean = ", round(mean(Inverse_unknown_y), 4)),
      pos = 4)
-
+var(Inverse_unknown_y)
 
 # Inverse function + antithetic variates
-Control_unknown_y <- function(n = 10000, B = 10000, p = 1/3){
+Control_unknown_y <- function(n = 1000, B = 1000, p = 1/3){
   y <- numeric(B)
   for(i in 1:B){
     x <- first_car_control_var(n, p)
